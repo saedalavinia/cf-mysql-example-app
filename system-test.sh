86614aaa1897316d9557e33a595850a4511ee26b
Daniel Jones <daniel.jones@engineerbetter.com>
2017-06-28 15:46:59 +0100

    only require one domain

--- system-test.sh
+++ system-test.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 go build
 
-cf api --skip-ssl-validation $CF_API
+cf api --skip-ssl-validation https://api.$CF_DOMAIN
 cf auth $CF_USERNAME $CF_PASSWORD
 cf target -o $CF_ORG -s $CF_SPACE
 cf create-service $CF_SERVICE_NAME $CF_SERVICE_PLAN mysql-test
@@ -14,8 +14,8 @@ cf start mysql-example-app
 
 DATE=$(date)
 
-curl --fail -k -s -d "${DATE}" -X PUT https://mysql-example-app."${CF_APP_DOMAIN}"/some-key
-curl --fail -k -s https://mysql-example-app."${CF_APP_DOMAIN}"/some-key | grep -q "${DATE}"
+curl --fail -k -s -d "${DATE}" -X PUT https://mysql-example-app."${CF_DOMAIN}"/some-key
+curl --fail -k -s https://mysql-example-app."${CF_DOMAIN}"/some-key | grep -q "${DATE}"
 
 cf delete -f mysql-example-app
 cf delete-service -f mysql-test
